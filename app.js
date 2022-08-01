4c8b170b7ef1f4d9eae41f69415a921324abbf98
BottledBananas <rubentiehuis@msn.com>
2022-08-01 15:06:40 +0200

    added dynamic grid

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-function test() {
-  console.log("test");
+const container = document.getElementById("container");
+const setupBtn = document.querySelector("#setup");
+function makeRows(rows, cols) {
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  }
+}
+
+makeRows(32, 32);
+
+function selector() {
+  let input = prompt("How many squared pixels do you want the grid to be?");
+  if (input > 100) {
+    prompt(`${input} pixels is too much! Try less than 100 pixels.`);
+  }
+  makeRows(input, input);
 }
